c0a536afbf6c926b6270dacd8369481cfe05bf13
eclerchik13 <ler.poter2014@yandex.ru>
2021-03-29 14:33:42 +0300

    Added navigation, logos

--- App.tsx
+++ App.tsx
@@ -1,45 +1,127 @@
 import React from 'react';
+import { StyleSheet} from "react-native";
 import AuthPage from "./src/app/auth/Authorization";
 import RegPage from "./src/app/register/Register";
+import NewPage from "./src/app/pages/News";
+import {Drawer} from 'react-native-paper'
+import { Provider as PaperProvider } from 'react-native-paper';
+import { createDrawerNavigator, DrawerItem} from '@react-navigation/drawer';
 import {NavigationContainer} from "@react-navigation/native";
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createDrawerNavigator();
+
+const DrawerContent =({navigation})=>{
+    return(
+            <Drawer.Section style={styles.drawerSection} >
+                <DrawerItem
+                    icon={({ size }) => (
+                        <MaterialCommunityIcons
+                            name="home"
+                            color={'#ff0044'}
+                            size={size}
+                        />
+                    )}
+                    label="Новости"
+                    onPress={() => {navigation.navigate('Новости')}}
+                />
+                <DrawerItem
+                    icon={({ size }) => (
+                        <MaterialCommunityIcons name="tune" color={'#ff0044'} size={size} />
+                    )}
+                    label="Работы"
+                    onPress={() => {}}
+                />
+                <DrawerItem
+                    icon={({ size }) => (
+                        <Ionicons name="settings-sharp" color={'#ff0044'} size={size} />
+                    )}
+                    label="Настройки"
+                    onPress={() => {}}
+                />
+                <DrawerItem
+                    icon={({ size }) => (
+                        <MaterialCommunityIcons name="login" color={'#ff0044'} size={size}/>
+                        )}
+                    label="Вход"
+                    onPress={() => {navigation.navigate("Вход")}}
+                />
+                <DrawerItem
+                icon={({ size }) => (
+                    <Ionicons name="people" color={'#ff0044'} size={size}/>
+                )}
+                label="Регистрация"
+                onPress={() => {navigation.navigate("Регистрация")}}
+            />
+            </Drawer.Section>
+    )
+}
+
+const DabNavigation =()=>{
+    return(
+        <Tab.Navigator drawerContent={(props) => <DrawerContent {...props}/>}>
+            <Tab.Screen name={"Вход"} component={AuthPage}/>
+            <Tab.Screen name={'Регистрация'} component={RegPage}/>
+            <Tab.Screen name={'Новости'} component={NewPage}/>
+        </Tab.Navigator>
+    )
+}
 
 const App: React.FC = () => {
     return(
+        <PaperProvider>
             <NavigationContainer>
-                <Tab.Navigator barStyle={
-                    {backgroundColor: "red",
-                    padding: 5,
-                        display:"flex",
-                        alignItems: "center"
-                    }
-                }>
-                    <Tab.Screen name="Login" component={AuthPage} />
-                    <Tab.Screen name="Register" component={RegPage} />
-                </Tab.Navigator>
-        </NavigationContainer>
+            <DabNavigation />
+            </NavigationContainer>
+        </PaperProvider>
     )
 }
 
 export default App
 
-/*export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your APP!
-      Похоже, это прогресс...</Text>
-      <TextInput
-          style={{
-            height: 100,
-            width: 300,
-            borderColor: 'gray',
-            borderWidth: 1
-          }}
-          defaultValue="You can type in me"
-      />
-      <StatusBar style="dark" />
-    </View>
-  );
-}*/
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    drawerSection: {
+        flex: 1,
+        justifyContent: "center",
+        marginTop: 5
+    }
+});
+
+
+//options={{tabBarLabel: "Pop", tabBarIcon:()=>(<AntDesign name="login" size={25} color="black"/>)}}
+/*<Tab.Navigator barStyle={
+                    {   backgroundColor: "red",
+                        padding: 100,
+                        display:"flex",
+                        alignItems: "center",
+                    }
+                }>*/
+
+/*<NavigationContainer>
+                <Tab.Navigator>
+                    <Tab.Screen name="Login" component={AuthPage} />
+                    <Tab.Screen name="Register" component={RegPage}/>
+                </Tab.Navigator>;
+            </NavigationContainer>*/
+
+/*const TabNavigation: React.FC=()=>{
+    return(
+        <Tab.Navigator drawerStyle={{
+            backgroundColor: '#ffffff',
+        }} drawerContentOptions={{
+            activeBackgroundColor: "#f1f1f1",
+            activeTintColor: '#5622b6',
+            itemStyle: { width: 250 ,marginVertical: 5}
+        }}>
+            <Tab.Screen name={"Вход"} component={AuthPage}/>
+            <Tab.Screen name={'Регистрация'} component={RegPage}/>
+            <Tab.Screen name={'Новости'} component={NewPage}/>
+        </Tab.Navigator>
+    )
+}
+*/
