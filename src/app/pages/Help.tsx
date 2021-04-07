ca11bb66c6c41dcb3a1cfb1f7508108598f2f38a
eclerchik13 <ler.poter2014@yandex.ru>
2021-04-07 15:02:13 +0300

    Added new pages

---
+++ src/app/pages/Help.tsx
@@ -0,0 +1,175 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import Footer from "../common/Footer";
+import Header from "../common/Header";
+import { Foundation } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+interface Contacts{
+    number: string,
+    email: string,
+    telegram: string
+}
+
+const Text_ = {
+    date: "Обратитесь в нашу службу поддержки, если у Вас возникли трудности с работой в приложении."
+}
+
+const contact: Contacts = {
+    number: '+7 (495) 788 56 99 доб. 9642',
+    email: "admin@ikaf42.ru",
+    telegram: "@meloman42"
+}
+
+const Tel: React.FC = () => {
+    return(
+        <View style={styles.marg2}>
+            <Text style={styles.title_C}>Номер телефона</Text>
+            <Text>{contact.number}</Text>
+        </View>
+    )
+}
+
+const Email:React.FC = () => {
+    return(
+        <View style={styles.marg}>
+            <Text style={styles.title_C}>Почта</Text>
+            <Text>{contact.email}</Text>
+        </View>
+    )
+}
+
+const Telegram:React.FC = () => {
+    return (
+        <View style={styles.marg3}>
+            <Text style={styles.title_C}>Telegram</Text>
+            <Text>{contact.telegram}</Text>
+        </View>
+    )
+}
+const Contact: React.FC = () => {
+    return (
+        <View style={styles.container_C}>
+            <View style={styles.row}>
+                <Foundation name="telephone" size={50} color="black" />
+                <Tel/>
+            </View>
+            <View  style={styles.row}>
+                <MaterialIcons name="local-post-office" size={50} color="black" />
+               <Email/>
+            </View>
+            <View style={styles.row}>
+                <FontAwesome5 name="telegram-plane" size={50} color="black" />
+                <Telegram/>
+            </View>
+        </View>
+    )
+}
+
+const Help: React.FC = () => {
+    return(
+        <View style={styles.Htext}>
+            <Text style={styles.title}>Вам нужна наша помощь?</Text>
+            <Text style={styles.minititle}>{Text_.date}</Text>
+        </View>
+    )
+}
+
+const HelpForm: React.FC = () => {
+    return(
+        <SafeAreaView style={styles.container}>
+            <View>
+                <Help/>
+                <Contact/>
+                <StatusBar style="dark" />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const HelpPage: React.FC= () =>{
+    return(
+        <View style={styles.container}>
+            <Header title={"Поддержка"}/>
+                <HelpForm/>
+                <Footer/>
+        </View>
+
+    )
+}
+
+export default HelpPage
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        alignItems: 'center',
+        backgroundColor: "#f4f6f8"
+    },
+    container_C: {
+        marginBottom: 71
+    },
+    data:{
+        fontSize: 14,
+        margin:10,
+    },
+    title:{
+        fontSize: 25,
+        margin: 10,
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    minititle:{
+        fontSize: 15,
+        margin: 10,
+        color: "white",
+        fontWeight: "bold",
+        textAlign :"center"
+    },
+    time:{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        fontSize: 14,
+        marginLeft: 10,
+        marginBottom: 7,
+        marginTop: 7
+    },
+    row:{
+        marginTop: 10,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
+    },
+    Htext:{
+        marginTop: 80,
+        marginBottom: 20,
+        width:300,
+        height: 200,
+        alignItems: "center",
+        borderColor: '#3f51b5',
+        borderWidth: 1,
+        borderRadius: 10,
+        backgroundColor: "#3f51b5",
+        //flexDirection: "row",
+        justifyContent: "center"
+    },
+    title_C:{
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    marg:{
+        marginLeft: 11
+    },
+    marg2: {
+        marginLeft: 25
+    },
+    marg3: {
+        marginLeft: 17
+    }
+});
---
+++ src/app/pages/Job.tsx
@@ -0,0 +1,184 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {StyleSheet, Text, View, SafeAreaView, ScrollView} from 'react-native';
+import Footer from "../common/Footer";
+import Header from "../common/Header";
+import { ITime} from "./News"
+
+let Job_One: Vacancy =  {
+    data: "В среду Денис Макрушин снова приглашает на открытую лекцию.\n" +
+        "\n" +
+        "В этот раз вести её будет Александр Барабанов, Principal Security Architect в команде Huawei Advanced Security Research.\n" +
+        "\n" +
+        "Тема лекции - Безопасность микросервисной архитектуры и её типовые уязвимости.\n" +
+        "\n" +
+        "Topic: Denis Makrushin's Open Lecture \n" +
+        "Time: Nov 18, 2020 05:55 PM Moscow \n" +
+        " \n" +
+        "Join Zoom Meeting \n" +
+        "https://zoom.us/j/95098133837?pwd=eHBVYzlBSkhjTlM5NXgvYmF1SldxZz09",
+    title: "Открытая лекция Дениса Макрушина!",
+    shortInformation: "Совсем маленькая, но очень нужная информация",
+    date: '13.10.2021'
+}
+
+let Job_Two: Vacancy = {
+    data: "🔐 Компания КриптоПро – лидер разработки криптографических средств защиты информации и электронной подписи. Продуктом, стоящим в фундаменте всех наших разработок, является КриптоПро CSP."
+        +"\n\n"+"Это кроссплатформенный набор библиотек, приложений и служб, которые:"+ "\n" + "● реализуют криптографические алгоритмы и протоколы" + "\n" +
+        "● обеспечивают абстракцию от платформы " + "\n" +
+        "● реализуют поддержку инфраструктуры открытых ключей/сертификатов, токенов и смарт-карт" + "\n" +
+        "● предоставляют стандартизованные API для встраивания (CryptoAPI, CNG, PKCS#11, openssl) и GUI/TUI для людей"+ "\n" +
+        "● успешно встроены в MS IE, MS IIS, MS Outlook, MS IPsec, MS EFS, Chromium, Firefox, openssl, ngnix, Apache и проч." + "\n\n" +
+        "Всё это нужно для:" + "\n" +
+        "● защиты государственных информационных систем" + "\n" +
+        "● безопасного доступа к государственным порталам" + "\n" +
+        "● электронных торгов и электронной налоговой отчётности" + "\n" +
+        "● работы системы электронных паспортов (пока заграничных)"+ "\n" +
+
+        "💫 В процессе работы вы:" + "\n" +
+        "● поучаствуете в промышленной разработке бок о бок с лучшими специалистами в отрасли" + "\n" +
+        "● познакомитесь с ОС Window/Linux/FreeBSD/Solaris/AIX/macOS/iOS/Android/Sailfish" + "\n" +
+        "● поработаете на Intel/AMD/PowerPC/ARM/MIPS/Эльбрус" + "\n" +
+        "● узнаете про POSIX/SVN/Jenkins/gdb/WinDbg/wxWidgets/cppunit/TDD/CI и многое другое" + "\n\n" +
+
+        "📚 Требования:"+ "\n" +
+        "● высшее техническое или математическое образование (в том числе студенты старших курсов)" + "\n" +
+        "● понимание принципов устройства операционных систем Windows/*nix"+ "\n" +
+        "● навыки разработки на Си, в меньшей степени – на C++ (Привет, Ваня!)"+ "\n\n" +
+        "🚀 Условия работы:"+ "\n" +
+        "Умирать каждый день" + "\n\n" +
+
+        "📲 Для связи:"+"\n"+"raa@cryptopro.ru - Русев Андрей Андреевич, начальник отдела системных разработок"+ "\n\n" +
+
+        "Все вакансии компании: https://www.cryptopro.ru/job",
+        title: "Вакансия/стажировка в \"КриптоПро\" [2]",
+        shortInformation: 'Младший разработчик на С/С++ в отдел системных раз',
+        date: '13.10.2021'
+}
+
+interface Vacancy {
+    title: string,
+    shortInformation: string,
+    data: string,
+    date: string
+}
+
+
+const Job: React.FC<Vacancy> = (props) => {
+    return(
+        <View style={styles.containerNew}>
+            <Text style={styles.title}>{props.title}</Text>
+            <Text style={styles.shortInformation}>{props.shortInformation}</Text>
+            <Text style={styles.data}>{props.data}</Text>
+            <View style={styles.cherta}/>
+            <ITime date={props.date}/>
+        </View>
+    )
+}
+
+const NewForm: React.FC = () => {
+    return(
+
+        <SafeAreaView style={styles.container}>
+            <View>
+                <Job data={Job_One.data}
+                     title={Job_One.title} date={Job_One.date} shortInformation={Job_One.shortInformation}/>
+                <Job data={Job_Two.data}
+                     title={Job_Two.title} date={Job_Two.date} shortInformation={Job_Two.shortInformation}/>
+                <StatusBar style="dark" />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const JobPage: React.FC= () =>{
+    return(
+
+        <View style={styles.container}>
+            <Header title={"Вакансии"}/>
+            <ScrollView>
+                <NewForm />
+                <Footer/>
+            </ScrollView>
+        </View>
+
+    )
+}
+
+export default JobPage
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: 'center',
+        backgroundColor: "#f4f6f8"
+    },
+    containerNew:{
+        borderColor: '#fafafa',
+        borderWidth: 1,
+        backgroundColor: "#fafafa",
+        borderRadius: 10,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 4,
+        margin: 10
+    },
+    button:{
+        backgroundColor: "#ff0044",
+        borderRadius: 7,
+        width: 300,
+        margin: 10,
+        alignItems: 'center',
+        height: 60
+    },
+    textInput: {
+        height: 60,
+        width: 300,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius:7,
+        margin: 5,
+    },
+    data:{
+        fontSize: 14,
+        margin:10,
+    },
+    title:{
+        fontSize: 25,
+        margin: 10
+    },
+    date:{
+        alignItems: 'center',
+        position: "relative",
+        //color: "#5623B6",
+        fontWeight: "bold",
+        marginLeft: 7
+    },
+    icon:{
+
+    },
+    time:{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        fontSize: 14,
+        marginLeft: 10,
+        marginBottom: 7,
+        marginTop: 7
+    },
+    cherta:{
+        borderBottomWidth: 1,
+        borderBottomColor: "#dbdbdb"
+    },
+    shortInformation: {
+        color: "grey",
+        fontSize: 12,
+        marginLeft: 10
+    }
+});
